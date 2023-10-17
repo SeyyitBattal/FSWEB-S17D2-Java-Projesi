c2a74879efb403b6e60177683b072b9fbcbc5550
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-17 12:28:19 +0300

    Project set up

---
+++ src/test/java/com/example/spring/daytwo/DaytwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.daytwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaytwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
